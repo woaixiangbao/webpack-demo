190dba05c9c20cd2b8d913d6d0dfef619847143f
jianqing <blessjq@gmail.com>
2018-11-05 10:09:54 +0800

    静态文件的添加与loader配置

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+
 function component() {
-    var element = document.createElement('div');
+  var element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+  return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|ttf|woff1|eot|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
 };
