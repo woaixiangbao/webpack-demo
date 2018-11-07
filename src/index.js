4c97afea546758778ab0dc321d59a22276f05281
jianqing <blessjq@gmail.com>
2018-11-07 10:29:20 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
 
+  var btn = document.createElement('button');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
   element.classList.add('hello');
   var myIcon = new Image();
   myIcon.src = Icon;
