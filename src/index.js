d37d431131e00a63fcfe6c0ddececd752813a5cf
jianqing <blessjq@gmail.com>
2018-11-25 17:58:47 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import {cube} from './math.js';
+import _ from 'lodash';
+
+console.log('现在的环境变量是：'+ process.env.NODE_ENV);
 
 function component() {
-
+  console.log(
+    _.join('1111', '2222', '3333!', '')
+  );
   var element = document.createElement('pre');
   element.innerHTML = [
     'Hello webpack!', 
---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    another: './src/another-module.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Production'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|ttf|woff1|eot|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
